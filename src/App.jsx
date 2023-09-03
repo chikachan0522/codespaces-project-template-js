8b41045833d42670153bf50957edfb7514d6eecf
chikachan <info@chikachan.info>
2023-09-03 13:43:09 +0000

    change personal info

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "chikachan",
+  title: "High school student",
+  email: "info@chikachan.info",
+  gitHub: "chikachan0522",
+  instagram: "chikachan_0522",
+  linkedIn: "",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "@chikachan_0522",
+  youTube: "@chikachan_0522",
 };
 
 const primaryColor = "#4E567E";
